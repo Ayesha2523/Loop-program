8fb0108bc195a74539987c3a43953c32b2c28002
Ayesha2523 <111120173+Ayesha2523@users.noreply.github.com>
2023-07-04 19:57:51 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,38 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+
+int a,b=1;
+
+cout<<"Enter the value=";
+
+cin>>b;
+
+for(a=1;a<=10;a++){
+    
+      cout<<b<<"x"<<a<<"="<<a*b<<"\n"; 
+    
+}
+
+
+    // cout<<b<<"x"<<a<<"="<<a*b<<""; 
+  
+
+cout<<endl;
+
+
+
+
+}
